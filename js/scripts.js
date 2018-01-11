a6c77a1eb01ef471f0db3886063bde26cf5255d9
Ainur Dzhaianbaeva and Faiza Husain <ainur@>
2018-01-11 13:43:04 -0800

    made correction on our script

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,33 @@
+
+
 $(document).ready(function() {
-  var animal = prompt("What's your favorite animal?");
+
+
+
+  var animal = prompt("What is your favorite animal? Cats, Pandas, or Dogs?");
+  console.log(animal);
 
   if ((animal === "cat") || (animal === "cats")) {
+    console.log("HELLO");
     $("#cats").show();
-
   } else if ((animal === "panda") || (animal === "pandas")) {
     $("#pandas").show();
-
-  } else if ((animal === "dog") || (animal ==="dogs")) {
+  } else if ((animal === "dog") || (animal === "dogs")) {
     $("#dogs").show();
-
   } else {
-    alert("Please select one of three animals: cats, pandas, dogs");
+    alert("Please select an animal from the following: cats, pandas, dogs.");
   }
 
-    $(".cat-clicakable").click(function () {
-
-      ("#cats").toggle();
-
-    )};
-
-    // $(".pandas-clicakable").click(function () {
-    //   ("#pandas").toggle();
-      $(".pandas-clicakable").click(function () {
-        ("#pandas").toggle();
-      });
-
-
-    // $(".dogs-clicakable").click(function () {
-    //     ("#dogs").toggle();
-
-      $(".dogs-clicakable").click(function () {
-        ("#dogs").toggle();
-      });
+  $(".cat-clicakable").click(function () {
+    $("#cats").toggle();
   });
+
+  $(".pandas-clicakable").click(function () {
+    $("#pandas").toggle();
+  });
+
+  $(".dogs-clicakable").click(function () {
+    $("#dogs").toggle();
+  });
+
 });
