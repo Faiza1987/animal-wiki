98a6cd4dc7dc8de936cc74b78c31ebdbba79e76c
Ainur Dzhaianbaeva and Faiza Husain <ainur@>
2018-01-11 14:38:53 -0800

    add changes in cat information

--- js/scripts.js
+++ js/scripts.js
@@ -18,15 +18,23 @@ $(document).ready(function() {
     alert("Please select an animal from the following: cats, pandas, dogs.");
   }
 
-  $(".cat-clicakable").click(function () {
-    $("#cats").toggle();
+  $("#cat1").click(function () {
+    $(".cat-clickable1").click();
+    $("#catimg1").show();
+    $("#cat-info1").toggle();
   });
 
-  $(".pandas-clicakable").click(function () {
+  $("#cat2").click(function () {
+    $(".cat-clickable2").click();
+    $("#catimg2").show();
+    $("#cat-info2").toggle();
+  });
+
+  $(".pandas-clickable").click(function () {
     $("#pandas").toggle();
   });
 
-  $(".dogs-clicakable").click(function () {
+  $(".dogs-clickable").click(function () {
     $("#dogs").toggle();
   });
 
